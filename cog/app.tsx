1a3eccb30fdf9e1201963bd8946537a5b21a285a
senthil <senthilnathan14062000@gmail.com>
2023-03-01 11:00:26 +0530

    added routing

--- cog/app.tsx
+++ cog/app.tsx
@@ -1,10 +1,11 @@
 import React, {FC} from 'react'
+import MainRoutes from './MainRoutes/MainRoutes'
 
 const App: FC = () => {
 
   return (
     <>
-    <h1>Build with ReactJS</h1>
+      <MainRoutes/>
     </>
   )
 }
