3ee112786835483af42af8d49a51fb3f9b8e0885
jyoti92-korde <117429213+jyoti92-korde@users.noreply.github.com>
2023-03-01 16:08:41 +0530

    signup page ui

--- cog/MainRoutes/MainRoutes.tsx
+++ cog/MainRoutes/MainRoutes.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 const Login = React.lazy(() => import('../components/screens/Login/Login'))
+const SignUp = React.lazy(() => import('../components/screens/SignUp/SignUp'))
 const MainRoutes = () => {
   return (
         <React.Suspense>
             <Routes>
                 <Route path='' element={<Login/>}/>,
                 <Route path='/login' element={<Login/>}/>,
+                <Route path='/signup' element={<SignUp/>}/>,
                 <Route path='*' element={<Login/>}/>
             </Routes>
         </React.Suspense>
   )
 }
-
+ 
 export default MainRoutes
