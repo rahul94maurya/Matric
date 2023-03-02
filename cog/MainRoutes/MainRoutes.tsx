450d3ca5fe2ab42f1a9265e8523a90f569e3274b
rahul <rahul.m@thinkbridge.in>
2023-03-02 15:19:14 +0530

    forgot password page UI

--- cog/MainRoutes/MainRoutes.tsx
+++ cog/MainRoutes/MainRoutes.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import ForgotPassword from "../components/screens/Forgot-password/ForgotPassword";
 
-const Login = React.lazy(() => import('../components/screens/Login/Login'))
+const Login = React.lazy(() => import("../components/screens/Login/Login"));
 const MainRoutes = () => {
   return (
-        <React.Suspense>
-            <Routes>
-                <Route path='' element={<Login/>}/>,
-                <Route path='/login' element={<Login/>}/>,
-                <Route path='*' element={<Login/>}/>
-            </Routes>
-        </React.Suspense>
-  )
-}
+    <React.Suspense>
+      <Routes>
+        <Route path="" element={<Login />} />,
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />,
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </React.Suspense>
+  );
+};
 
-export default MainRoutes
+export default MainRoutes;
---
+++ cog/components/screens/Forgot-password/ForgotPassword.tsx
@@ -0,0 +1,58 @@
+import React, { useRef } from "react";
+import { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const emailInputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    // send password reset email to user's email
+    setShowAlert(true);
+    if (emailInputRef.current != null) {
+      emailInputRef.current.value = "";
+    }
+  };
+
+  return (
+    <div className="login-container card">
+      <div className="row m-0">
+        <div className="col-lg-12">
+          <h3 className="d-flex align-items-center justify-content-center">
+            Forgot Password
+          </h3>
+        </div>
+        {showAlert && (
+          <Alert
+            variant="success"
+            onClose={() => setShowAlert(false)}
+            dismissible
+          >
+            Password reset email has been sent to {email}.
+          </Alert>
+        )}
+        <div className="col-lg-12">
+          <Form>
+            <div className="form-group">
+              <div className="mt-3 fw-bold form-label">Email Address</div>
+              <input
+                type="email"
+                placeholder="Enter your email address"
+                className="form-control"
+                ref={emailInputRef}
+                onChange={(e: any) => setEmail(e.target.value)}
+              />
+              <Button className="mt-3 w-100 common-btn" onClick={handleSubmit}>
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPassword;
