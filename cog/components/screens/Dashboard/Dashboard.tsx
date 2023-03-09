0bc9c24bf4fa696f4a76f9dab0bc651eefc8387e
senthil <senthilnathan14062000@gmail.com>
2023-03-09 16:51:59 +0530

    redux setup

--- cog/components/screens/Dashboard/Dashboard.tsx
+++ cog/components/screens/Dashboard/Dashboard.tsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {  useDispatch, useSelector } from 'react-redux'
+import { fetchData } from '../../../redux/reducer/airLineSlice'
+
+
+export interface airlineData{
+  data :[]
+}
 
 const Dashboard = () => {
+    // const data = useSelector<currentData,currentData[]>(((state) => state.currentData))
+    // const data = useSelector<RootState,string>((state:airlineData) => state.airline)
+    // console.log("data",data)
+    const data = useSelector((state:airlineData) => state.data)
+    console.log("data",data)
+    const dispatch = useDispatch()
+
+    // useEffect(() => {
+    //   dispatch(fetchData())
+    // },[])
+  
   return (
     <>
         Dashboard
--- cog/index.tsx
+++ cog/index.tsx
@@ -6,11 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './style/style.css'
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const container = document.getElementById('app-root')!
 const root = createRoot(container)
 root.render(
       <BrowserRouter>
+      <Provider store={store}>
         <App/>
+      </Provider>
       </BrowserRouter>
   )
