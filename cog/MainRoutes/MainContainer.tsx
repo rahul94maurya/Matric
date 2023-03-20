867d4bf210daf87c66d764bb3cf62a29d21512d4
senthil <senthilnathan14062000@gmail.com>
2023-03-20 11:16:43 +0530

    sidepanel wrapper component

--- cog/MainRoutes/MainContainer.tsx
+++ cog/MainRoutes/MainContainer.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from '../components/common/Header/Header'
 import Navbar from '../components/common/Navbar/Navbar'
+import SidePanel from '../components/common/SidePanel/SidePanel'
 
 
 const MainContainer = () => {
+    const toggleSwitch = useRef<HTMLDivElement>(null)
+    const onItemClick = () => {
+        const navigationBar = document.getElementsByClassName('open-btn')[0]
+        if (toggleSwitch.current && navigationBar) {
+            toggleSwitch.current.classList.add("active")
+        }
+
+    }
+
+    const onRemoveClick = () => {
+        const mainContent = document.getElementsByClassName('close-btn')[0]
+        if (toggleSwitch.current && mainContent) {
+            toggleSwitch.current.classList.remove("active")
+        }
+    }
     return (
         <>
-        <Navbar/>
+            {/* <Navbar/> */}
+            <div className='main-container d-flex'>
+                <div className='sidebar ' id='side_nav' ref={toggleSwitch}>
+                    <SidePanel onRemoveClick={onRemoveClick} />
+                </div>
+                <div className='content'>
+                    <Header onItemClick={onItemClick} />
+                    <div className='me-1 p-3 ms-2 mt-3 page-content'>
+                        <Outlet />
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
